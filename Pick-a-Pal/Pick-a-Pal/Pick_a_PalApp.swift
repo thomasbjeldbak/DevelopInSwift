b1675de2eb8763c23f49ae8f6e2bf7aa5e29fea6
Thomas Bjeldbak Madsen <75805327+thomasbjeldbak@users.noreply.github.com>
2024-05-06 19:12:49 +0200

    Pick-a-Pal done

---
+++ Pick-a-Pal/Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Thomas Bjeldbak on 06/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
